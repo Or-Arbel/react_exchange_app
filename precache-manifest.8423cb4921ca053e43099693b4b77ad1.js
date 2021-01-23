c80f3be8a354f61f0840388225157d5b1b520103
Or-Arbel <orarbel1995@gmail.com>
2021-01-23 19:13:03 +0200

    Updates

---
+++ precache-manifest.8423cb4921ca053e43099693b4b77ad1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4044c486a6c2cba84fe15141e32208f1",
+    "url": "/react_exchange_app/index.html"
+  },
+  {
+    "revision": "aec7e1a90cfdd29b6ce4",
+    "url": "/react_exchange_app/static/css/main.e0c1a898.chunk.css"
+  },
+  {
+    "revision": "f60ac148a9051ec40725",
+    "url": "/react_exchange_app/static/js/2.7eff08ee.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react_exchange_app/static/js/2.7eff08ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aec7e1a90cfdd29b6ce4",
+    "url": "/react_exchange_app/static/js/main.0b15d222.chunk.js"
+  },
+  {
+    "revision": "f813f28c281bc826f718",
+    "url": "/react_exchange_app/static/js/runtime-main.facf1189.js"
+  }
+]);
